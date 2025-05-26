d2fa25826aace6f3ad3501147805b8106deffaab
zachattackm98 <zachm98@hotmail.com>
2025-05-25 18:59:39 -0700

    Fix Router context error

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { ClerkProvider } from '@clerk/clerk-react';
-import { useNavigate } from 'react-router-dom';
+import { BrowserRouter, useNavigate } from 'react-router-dom';
 import App from './App.tsx';
 import './index.css';
 
@@ -31,6 +31,8 @@ function ClerkProviderWithRoutes() {
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ClerkProviderWithRoutes />
+    <BrowserRouter>
+      <ClerkProviderWithRoutes />
+    </BrowserRouter>
   </StrictMode>
 );
